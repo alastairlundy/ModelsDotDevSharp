42c409dcf9ce5e8293bbae319e967dcc1b6e8bd2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-06-18 11:40:57 +0100

    Add AddModelsDev DI extension and update GlobalUsings.cs
    - Add ModelsDevServiceCollectionExtensions.cs with AddModelsDev method that registers ModelsDevOptions and three repositories as singletons
    - Update GlobalUsings.cs to import ModelsDotDevSharp.Converters, ModelsDotDevSharp, and Microsoft.Extensions.Options
    
    Co-authored by opencode: Deepseek V4 Flash

--- src/ModelsDotDevSharp/GlobalUsings.cs
+++ src/ModelsDotDevSharp/GlobalUsings.cs
@@ -2,5 +2,8 @@
 
 global using System.ComponentModel;
 global using System.Text.Json.Serialization;
+global using Microsoft.Extensions.Options;
+global using ModelsDotDevSharp;
 global using ModelsDotDevSharp.Abstractions;
 global using ModelsDotDevSharp.Contexts;
+global using ModelsDotDevSharp.Converters;
